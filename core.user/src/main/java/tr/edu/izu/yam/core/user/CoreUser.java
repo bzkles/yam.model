dd9fcdd5c98386b54224b015c216e8f28327c22b
Kevser Arıca <kevserarica@hotmail.com>
2019-05-16 09:56:28 +0300

    Getter ve Setter metodları düzeltildi

--- core.user/src/main/java/tr/edu/izu/yam/core/user/CoreUser.java
+++ core.user/src/main/java/tr/edu/izu/yam/core/user/CoreUser.java
@@ -58,4 +58,84 @@ public class CoreUser implements Serializable {
 	@Basic
 	@Column(name = "EXPIREDATE", nullable = true)
 	private Date expireDate;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEncrypted() {
+		return encrypted;
+	}
+
+	public void setEncrypted(String encrypted) {
+		this.encrypted = encrypted;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getClear() {
+		return clear;
+	}
+
+	public void setClear(String clear) {
+		this.clear = clear;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getExpireDate() {
+		return expireDate;
+	}
+
+	public void setExpireDate(Date expireDate) {
+		this.expireDate = expireDate;
+	}
 }
